2c2587198d1c46d74373a1b9029c1980f6b00dc3
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-01-23 02:10:27 +0600

    Downloading resource and adding configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        moulik: "#FF4240",
+        kala: "#131318",
+        halkaKala: "#13131899"
+      },
+    },
+  },
+  plugins: [],
+};
